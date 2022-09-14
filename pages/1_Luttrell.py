df1a32a1f3b917ef1c70ad40e3acf156e735d44c
liz-kervick <110185521+liz-kervick@users.noreply.github.com>
2022-09-13 21:20:50 -0400

    Update 1_Luttrell.py

--- pages/1_Luttrell.py
+++ pages/1_Luttrell.py
@@ -15,8 +15,8 @@ import folium as folium
 #     return df_sum
     
     
-basepath= "streamlit_luttrell"
-rawpath= "/files/"
+basepath= ""
+rawpath= "files/"
 filename_df_sum= "luttrell_years_metadata.csv"
 filename_df_all='luttrell_by_entry_dates_metadata_sources_coords_ffill-7.csv'
 
